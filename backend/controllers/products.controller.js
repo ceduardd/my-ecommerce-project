29b0087959bece176cc5d94da684a6f2c6952216
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-11-08 23:56:16 -0500

    redux setup store created

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,12 +1,18 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel';
 
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 export const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
 
   res.json(products);
 });
 
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
 export const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,17 +1,11 @@
 import config from '../config';
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
 export const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
 export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
